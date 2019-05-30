8aaaf3ce62192e95278cc3d04d532a0a65039707
Bilevych Volodymyr <volodymyr_bilevych@epam.com>
2019-05-30 13:52:40 +0300

    new fragment for dialog

--- app/src/main/java/com/anblak/placesapp/ItemDetailActivity.java
+++ app/src/main/java/com/anblak/placesapp/ItemDetailActivity.java
@@ -1,15 +1,18 @@
 package com.anblak.placesapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
@@ -42,7 +45,77 @@ public class ItemDetailActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
+        addRatingListener();
+        addCommentListener();
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (savedInstanceState == null) {
+            // Create the detail fragment and add it to the activity
+            // using a fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(ItemDetailFragment.ARG_ITEM_ID,
+                    getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID));
+            ItemDetailFragment fragment = new ItemDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.item_detail_container, fragment)
+                    .commit();
+        }
+    }
+
+    private void addCommentListener() {
         FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setView(getLayoutInflater().inflate(R.layout.fragment_create_comment,null));
+                builder.setPositiveButton(R.string.done, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(LoginRepository.getUser() != null) {
+                            String message = ((EditText)findViewById(R.id.edit_comment)).getEditableText().toString();
+                            RequestBody formBody = new FormBody.Builder()
+                                    .add("message", message)
+                                    .add("uuid", LoginRepository.getUser().getUuid())
+                                    .add("placeId", String.valueOf(ItemDetailFragment.getmItem().getId()))
+                                    .build();
+
+                            Request request = new Request.Builder()
+                                    .url(Constants.SERVER_URL + "/comments")
+                                    .post(formBody)
+                                    .build();
+                            try {
+                                Response response = okHttpClient.newCall(request).execute();
+                                if(!response.body().string().equals(SUCCESS_RESULT)) {
+                                    Toast.makeText(ItemDetailActivity.this, "Error",
+                                            Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(ItemDetailActivity.this, "Comment: " + message,
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            } catch (IOException e) {
+                                Log.v(TAG, e.getMessage());
+                                Toast.makeText(ItemDetailActivity.this, "Error",
+                                        Toast.LENGTH_LONG).show();
+                            }
+
+                        } else {
+                            Toast.makeText(ItemDetailActivity.this, "Authorize please",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+                builder.create().show();
+            }
+        });
+    }
+
+    private void addRatingListener() {
         final RatingBar defaultRatingBar = findViewById(R.id.ratingBar_default);
         defaultRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
 
@@ -83,32 +156,6 @@ public class ItemDetailActivity extends AppCompatActivity {
                 }
             }
         });
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString(ItemDetailFragment.ARG_ITEM_ID,
-                    getIntent().getStringExtra(ItemDetailFragment.ARG_ITEM_ID));
-            ItemDetailFragment fragment = new ItemDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.item_detail_container, fragment)
-                    .commit();
-        }
     }
 
     @Override
